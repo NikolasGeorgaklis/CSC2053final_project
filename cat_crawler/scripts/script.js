bf288a036d30471b5be986b4ef3d5adb238a665f
Xavier Jibril Goudeaux <xgoudeau@villanova.edu>
2022-05-01 19:03:33 -0400

    Updated it. Should show js

--- cat_crawler/scripts/script.js
+++ cat_crawler/scripts/script.js
@@ -3,75 +3,152 @@ const context = document.querySelector("canvas").getContext("2d");
 context.canvas.height = 400;
 context.canvas.width = 1220;
 
-const character = {
-    height: 32,
-    width: 32,
-    
-    jumping: true,
-    x: 0,
-    xVelocity: 0,
-    y: 0,
-    yVelocity: 0
+let frameCount = 1;
+
+let obCount = frameCount;
+
+const obXCoors = [];
+
+const player = {
+
+  height: 32,
+  jumping: true,
+  width: 32,
+  x: 0,
+  xVelocity: 0,
+  y: 0,
+  yVelocity: 0
+
 };
 
+const nextFrame = () => {
+
+  frameCount++;
+  
+  for (let i = 0; i < obCount; i++) {
+    // Randomly generate the x coordinate for the top corner start of the obstacle
+    obXCoor = Math.floor(Math.random() * (1165 - 140 + 1) + 140);
+    obXCoors.push(obXCoor);
+  }
+
+}
+
 const controller = {
-    left: false,
-    right: false,
-    up: false,
-    keyListener: function (event) {
-      let key_state = (event.type == "keydown") ? true : false;
-      switch (event.keyCode) {
-        case 37: // left arrow
-          controller.left = key_state;
-          break;
-        case 38: // up arrow
-          controller.up = key_state;
-          break;
-        case 39: // right arrow
-          controller.right = key_state;
-          break;
-      }
-    }
-  };
 
-  const loop = function () {
-    if (controller.up && character.jumping == false) {
-      character.yVelocity -= 20;
-      character.jumping = true;
+  left: false,
+  right: false,
+  up: false,
+  keyListener: function (event) {
+
+    var key_state = (event.type == "keydown") ? true : false;
+
+    switch (event.keyCode) {
+
+      case 37:// left key
+        controller.left = key_state;
+        break;
+      case 38:// up key
+        controller.up = key_state;
+        break;
+      case 39:// right key
+        controller.right = key_state;
+        break;
+
     }
-    if (controller.left) {
-      character.xVelocity -= 0.5;
-    }
-    if (controller.right) {
-      character.xVelocity += 0.5;
-    }
+
   }
 
-  character.yVelocity += 1.5; // gravity
-  character.x += character.xVelocity;
-  character.y += character.yVelocity;
-  character.xVelocity *= 0.9; // friction
-  character.yVelocity *= 0.9; // friction
+};
 
+const loop = function () {
 
-  if (character.x < -20) {
-    character.x = 1220;
-  } else if (character.x > 1220) {  // if the square goes off the right
-    character.x = -20;
+  if (controller.up && player.jumping == false) {
+
+    player.yVelocity -= 20;
+    player.jumping = true;
+
   }
 
-// Creates the "ground" for each frame
-    context.strokeStyle = "#2E2532";
-    context.lineWidth = 30;
+  if (controller.left) {
+
+    player.xVelocity -= 0.5;
+
+  }
+
+  if (controller.right) {
+
+    player.xVelocity += 0.5;
+
+  }
+
+  player.yVelocity += 1.5;// gravity
+  player.x += player.xVelocity;
+  player.y += player.yVelocity;
+  player.xVelocity *= 0.9;// friction
+  player.yVelocity *= 0.9;// friction
+
+  // if player is falling below floor line
+  if (player.y > 386 - 16 - 32) {
+
+    player.jumping = false;
+    player.y = 386 - 16 - 32;
+    player.yVelocity = 0;
+
+  }
+
+  // if player is going off the left of the screen
+  if (player.x < -20) {
+
+    player.x = 1220;
+
+  } else if (player.x > 1220) {// if player goes past right boundary
+
+    player.x = -20;
+    nextFrame();
+
+  }
+  // Creates the backdrop for each frame
+  context.fillStyle = "#201A23";
+  context.fillRect(0, 0, 1220, 400); // x, y, width, height
+
+
+  // Creates and fills the cube for each frame
+  context.fillStyle = "#8DAA9D"; // hex for cube color
+  context.beginPath();
+  context.rect(player.x, player.y, player.width, player.height);
+  context.fill();
+
+
+  // Create the obstacles for each frame
+  // Set the standard obstacle height
+  const height = 200 * Math.cos(Math.PI / 6);
+
+  context.fillStyle = "#FBF5F3"; // hex for triangle color
+  obXCoors.forEach((obXCoor) => {
     context.beginPath();
-    context.moveTo(0, 385);
-    context.lineTo(1220, 385);
-    context.stroke();
 
-    window.requestAnimationFrame(loop); // Updates when called to tell the browser it is ready to draw again
+    context.moveTo(obXCoor, 385); // x = random, y = coor. on "ground"
+    context.lineTo(obXCoor + 20, 385); // x = ^random + 20, y = coor. on "ground"
+    context.lineTo(obXCoor + 10, 510 - height); // x = ^random + 10, y = peak of triangle
+  
+    context.closePath();
+    context.fill();
+  })
 
+
+  // Creates the "ground" for each frame
+  context.strokeStyle = "#2E2532";
+  context.lineWidth = 30;
+  context.beginPath();
+  context.moveTo(0, 385);
+  context.lineTo(1220, 385);
+  context.stroke();
+
+  // call update when the browser is ready to draw again
+  window.requestAnimationFrame(loop);
+
+};
 
 window.addEventListener("keydown", controller.keyListener)
 window.addEventListener("keyup", controller.keyListener);
-
 window.requestAnimationFrame(loop);
