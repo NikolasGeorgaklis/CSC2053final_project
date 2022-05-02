ef597c226f24876808513ad4f5543324c9cc12c5
Xavier Jibril Goudeaux <xgoudeau@villanova.edu>
2022-05-02 13:37:24 -0400

    Resized cat and obstacles. js update
    jump made higher
    will is larger
    balls are larger //will add lines to them

--- cat_crawler/scripts/script.js
+++ cat_crawler/scripts/script.js
@@ -3,6 +3,13 @@ const context = document.querySelector("canvas").getContext("2d");
 context.canvas.height = 400;
 context.canvas.width = 1220;
 
+var cat = new Image()
+cat.src = 'images/will_d_cat.png';
+
+function drawCat(){
+  context.drawImage(cat,player.x,player.y,player.width,player.height)
+}
+
 
 let frameCount = 1;
 
@@ -13,9 +20,9 @@ const obXCoors = [];
 const player = {
 
   
-  height: 32,
+  height: 110,
   jumping: true,
-  width: 32,
+  width: 90,
   x: 0,
   xVelocity: 0,
   y: 0,
@@ -25,12 +32,6 @@ const player = {
 
 };
 
-var cat = new Image()
-cat.src = 'images/will_d_cat.png';
-
-function drawCat(){
-  context.drawImage(cat,player.x,player.y,player.width,player.height)
-}
 
 const nextFrame = () => {
 
@@ -77,20 +78,20 @@ const loop = function () {
 
   if (controller.up && player.jumping == false) {
 
-    player.yVelocity -= 20;
+    player.yVelocity -= 33;
     player.jumping = true;
 
   }
 
   if (controller.left) {
 
-    player.xVelocity -= 0.40;
+    player.xVelocity -= 0.5;
 
   }
 
   if (controller.right) {
 
-    player.xVelocity += 0.40;
+    player.xVelocity += 0.5;
 
   }
 
@@ -101,18 +102,18 @@ const loop = function () {
   player.yVelocity *= 0.9;// friction
 
   // if player is falling below floor line
-  if (player.y > 386 - 16 - 32) {
+  if (player.y > 386 - 16 - player.height) {
 
     player.jumping = false;
-    player.y = 386 - 16 - 32;
+    player.y = 386 - 16 - player.height;
     player.yVelocity = 0;
 
   }
 
   // if player is going off the left of the screen
-  if (player.x < -20) {
+  if (player.x < -25) {
 
-    player.x = -10;
+    player.x = -25;
 
   } else if (player.x > 1220) {// if player goes past right boundary
 
@@ -124,24 +125,19 @@ const loop = function () {
   context.fillStyle = "#6ccad6";
   context.fillRect(0, 0, 1220, 400); // x, y, width, height
 
-  // Creates and fills the cube for each frame
- drawCat();
-
-
+  // Creates will d cat for each frame
+  drawCat();
+  
   // Create the obstacles for each frame
   // Set the standard obstacle height
   const height = 200 * Math.cos(Math.PI / 6);
 
-  
-
   context.fillStyle = "#E47041"; // hex for triangle color
   obXCoors.forEach((obXCoor) => {
 
-    //collision field for obstacle
 
     context.moveTo(obXCoor, 385); // x = random, y = coor. on "ground"
-    context.arc(obXCoor, 345, 25, 0, 2 * Math.PI)
-  
+    context.arc(obXCoor, 322, 50, 0, 2 * Math.PI)
     context.closePath();
     context.fill();
   })
